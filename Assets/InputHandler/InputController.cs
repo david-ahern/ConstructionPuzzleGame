4714c7b034f224505f9062c9c9987b7e77d04e10
David Ahern <david-ahern@hotmail.co.uk>
2014-07-25 21:15:02 +0100

    Beginning of key bind controls

---
+++ Assets/InputHandler/InputController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class InputController : MonoBehaviour 
+{
+    void Update()
+    {
+        foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (!key.ToString().Contains("JoystickB"))
+                if (Input.GetKey(key))
+                    Debug.Log(key + " pressed");
+        }
+    }
+}
